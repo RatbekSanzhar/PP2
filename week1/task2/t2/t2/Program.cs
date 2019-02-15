4df39eecaaf29a9b06e7ec7dde63c6d540417e45
RatbekSanzhar <sanzhar.kan@gmail.com>
2019-02-15 22:10:55 +0600

    commit

--- week1/task2/t2/t2/Program.cs
+++ week1/task2/t2/t2/Program.cs
@@ -10,18 +10,27 @@ namespace t2
     {
         public string name; // a new field
         public int id; // a new field
-        public int year; // a new field
+        public int year=1; // a new field
 
         public Student(string name, int id) //creates a constructor
         {
             this.name = name; // assign a value
             this.id = id; // assign a value
         }
-
+        public string getName()
+        {
+            return name;
+        }
+        public int getId()
+        {
+            return id;
+        }
+        
         public void IncYear() // creates a method of increment of year
         {
             year++;
         }
+
     }
     class Program
     {
